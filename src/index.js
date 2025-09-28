e1ffc58f1cc218063022fd085196fc4da2773a07
akanematsubara <akane-in-takenoko.0807@outlook.jp>
2025-09-28 23:04:03 +0900

    表示位置調整

--- src/index.js
+++ src/index.js
@@ -15,10 +15,16 @@ const redirectPath = sessionStorage.getItem("redirectPath");
 if (redirectPath) {
   sessionStorage.removeItem("redirectPath");
   window.history.replaceState(null, null, redirectPath);
-  window.scrollTo(0, 0); // 最上部にスクロール
+  // React レンダリング後にスクロール
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 0); // 最上部にスクロール
 } else {
   // 通常ロードでも念のため最上部
-  window.scrollTo(0, 0);
+  // React レンダリング後にスクロール
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 0); // 最上部にスクロール
 }
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
